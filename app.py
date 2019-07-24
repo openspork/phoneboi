297549286ac0e862953019801fe61a68873f7480
christian <christianrgerlach@gmail.com>
2019-07-24 10:26:56 -0700

    basically working

--- app.py
+++ app.py
@@ -12,23 +12,55 @@ auth_header = {"Authorization":"Basic " + api_key,
 cw_url = "https://api-na.myconnectwise.net/v4_6_release/apis/3.0"
 
 def get_contacts(name):
-    query = "?conditions=firstName like '" + name + "' OR lastName like '" + name + "'"
+    query = "?conditions=firstName like '%" + name + "%' OR lastName like '%" + name + "%'"
     request_text = cw_url + "/company/contacts" + query
     r = get(request_text, headers=auth_header)
-    print(r.text)
     data = loads(r.text)
     return data
 
 @app.route('/<name>')
 def hello_world(name):
-    hello = {}
-    hello["type"] = "message"
-    hello["text"] = "Hello, World!"
+    return_data = {}
+    return_data["type"] = "message"
+    return_string = None
+
     contacts = get_contacts(name)
 
     for contact in contacts:
-        print(contact["firstName"] + " " + contact["lastName"])
+        return_line = ""
+        first = None
+        last = None
 
+        if "firstName" in contact.keys():
+            first = contact["firstName"]
 
+        if "lastName" in contact.keys():
+            last = contact["lastName"]
 
-    return dumps(hello)
+        if first:
+            return_line = first
+        if last:
+            if first:
+                return_line = return_line + " " + last
+            else:
+                return_line = last
+
+        if "communicationItems" in contact.keys():
+            comm_string = None
+            for comm_item in contact["communicationItems"]:
+                if not comm_string:
+                    comm_string = comm_item["type"]["name"] + ": " + comm_item["value"]
+                else:
+                    comm_string = comm_string + ", " + comm_item["type"]["name"] + " " + comm_item["value"]
+
+            if comm_string:
+                return_line = return_line + " - " + comm_string
+
+        if not return_string:
+            return_string = return_line
+        else:
+            return_string = return_string + "\n" + return_line
+
+    return_data["text"] = return_string
+
+    return dumps(return_data)
