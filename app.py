33ed74787ab364dbdd6d7d220fd49958cb256c06
christian <christianrgerlach@gmail.com>
2019-07-24 10:35:37 -0700

    formatting

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from json import dumps, loads
 from requests import get
 from flask import Flask
+
 app = Flask(__name__)
 
-with open('/home/christian/secrets/cw.token') as file:
-    api_key = file.read().replace('\n', '')
+with open("/home/christian/secrets/cw.token") as file:
+    api_key = file.read().replace("\n", "")
 
-auth_header = {"Authorization":"Basic " + api_key,
-             "Content-Type":"application/json"}
+auth_header = {"Authorization": "Basic " + api_key, "Content-Type": "application/json"}
 
 cw_url = "https://api-na.myconnectwise.net/v4_6_release/apis/3.0"
 
+
 def get_contacts(name):
-    query = "?conditions=firstName like '%" + name + "%' OR lastName like '%" + name + "%'"
+    query = (
+        "?conditions=firstName like '%" + name + "%' OR lastName like '%" + name + "%'"
+    )
     request_text = cw_url + "/company/contacts" + query
     r = get(request_text, headers=auth_header)
     data = loads(r.text)
     return data
 
-@app.route('/<name>')
+
+@app.route("/<name>")
 def hello_world(name):
     return_data = {}
     return_data["type"] = "message"
@@ -51,7 +55,13 @@ def hello_world(name):
                 if not comm_string:
                     comm_string = comm_item["type"]["name"] + ": " + comm_item["value"]
                 else:
-                    comm_string = comm_string + ", " + comm_item["type"]["name"] + " " + comm_item["value"]
+                    comm_string = (
+                        comm_string
+                        + ", "
+                        + comm_item["type"]["name"]
+                        + " "
+                        + comm_item["value"]
+                    )
 
             if comm_string:
                 return_line = return_line + " - " + comm_string
